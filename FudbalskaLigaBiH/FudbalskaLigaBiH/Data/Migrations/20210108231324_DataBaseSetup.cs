f7b94ecbd315061314da330afd006fea443df046
irma-maslesa <irma.maslesa@edu.fit.ba>
2021-01-09 00:39:25 +0100

    Project setup - DB and app deployment, CD

---
+++ FudbalskaLigaBiH/FudbalskaLigaBiH/Data/Migrations/20210108231324_DataBaseSetup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FudbalskaLigaBiH.Data.Migrations
+{
+    public partial class DataBaseSetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

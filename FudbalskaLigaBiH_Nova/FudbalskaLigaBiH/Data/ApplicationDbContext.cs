79ef055bf0e3812cd4dd8f2da2c576c6f1fe7ee7
irma-maslesa <irma.maslesa@edu.fit.ba>
2021-01-09 10:08:02 +0100

    Added new project

---
+++ FudbalskaLigaBiH_Nova/FudbalskaLigaBiH/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FudbalskaLigaBiH.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server = app.fit.ba,1431;
+                                          Database = p2036;
+                                          Trusted_Connection = False;
+                                          User ID = p2036;
+                                          Password = n?7i97Ek;
+                                          MultipleActiveResultSets = True;");
+        }
+    }
+}

b8356d4a6b52f15f8054c74753767f92388ab112
irma-maslesa <irma.maslesa@edu.fit.ba>
2021-01-09 22:59:46 +0100

    Closed #42 - Mogućnost prikaza i pretrage stadiona

---
+++ FudbalskaLigaBiH/FudbalskaLigaBiH/Controllers/StadionController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FudbalskaLigaBiH.Data;
+using FudbalskaLigaBiH.Models;
+
+namespace FudbalskaLigaBiH.Controllers
+{
+    public class StadionController : Controller
+    {
+        private ApplicationDbContext db;
+
+        public StadionController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult Prikaz(string filter)
+        {
+            StadionPrikazVM model = new StadionPrikazVM();
+
+            List<StadionPrikazVM.Row> stadioni = db.Stadion
+                                                   .Where(s => filter == null || filter == "" ||
+                                                   (s.Naziv.ToLower().StartsWith(filter.ToLower())) ||
+                                                   (s.Grad.Naziv.ToLower().StartsWith(filter.ToLower())))
+                                                   .Select(s => new StadionPrikazVM.Row
+                                                   {
+                                                       ID = s.ID,
+                                                       Naziv = s.Naziv,
+                                                       Kapacitet = s.Kapacitet,
+                                                       Grad = s.Grad.Naziv
+                                                   }).ToList();
+
+            model.Stadioni = stadioni;
+            model.filter = filter;
+
+            return View(model);
+        }
+    }
+}
---
+++ FudbalskaLigaBiH/FudbalskaLigaBiH/Models/StadionPrikazVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FudbalskaLigaBiH.Models
+{
+    public class StadionPrikazVM
+    {
+        public class Row
+        {
+            public int ID { get; set; }
+            public string Naziv { get; set; }
+            public int Kapacitet { get; set; }
+            public string Grad { get; set; }
+        }
+
+        public List<Row> Stadioni;
+        public string filter { get; set; }
+    }
+}

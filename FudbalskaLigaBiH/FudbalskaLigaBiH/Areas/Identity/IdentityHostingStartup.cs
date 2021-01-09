639ee53b504125f6c0d0ccb4d8e49778ba617525
AnisaSuljic <AnisaSuljic@users.noreply.github.com>
2021-01-09 19:57:33 +0100

    uredjene forme za prijavu i registraciju

---
+++ FudbalskaLigaBiH/FudbalskaLigaBiH/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FudbalskaLigaBiH.Data;
+using FudbalskaLigaBiH.EntityModels;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FudbalskaLigaBiH.Areas.Identity.IdentityHostingStartup))]
+namespace FudbalskaLigaBiH.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}

96639b8ad11d1de94966ee77868f9745b369758d
irma-maslesa <irma.maslesa@edu.fit.ba>
2021-01-09 15:42:29 +0100

    Added TrenerDodaj VM i V

---
+++ FudbalskaLigaBiH/FudbalskaLigaBiH/Controllers/TrenerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FudbalskaLigaBiH.Data;
+using Microsoft.AspNetCore.Mvc;
+using FudbalskaLigaBiH.Models;
+using FudbalskaLigaBiH.EntityModels;
+
+namespace FudbalskaLigaBiH.Controllers
+{
+    public class TrenerController : Controller
+    {
+        public IActionResult Dodaj()
+        {
+            return View();
+        }
+
+        public IActionResult Snimi(TrenerDodajVM t)
+        {
+            //ApplicationDbContext dbc = new ApplicationDbContext();
+
+            Trener trener = new Trener
+            {
+                Ime = t.Ime,
+                Prezime = t.Prezime,
+                Mail = t.Mail,
+                DatumRodjenja = t.DatumRodjenja
+            };
+
+
+            return Redirect("/");
+        }
+    }
+}
---
+++ FudbalskaLigaBiH/FudbalskaLigaBiH/Models/TrenerDodajVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FudbalskaLigaBiH.Models
+{
+    public class TrenerDodajVM
+    {
+        public int ID { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string Mail { get; set; }
+        public DateTime DatumRodjenja { get; set; }
+    }
+}
